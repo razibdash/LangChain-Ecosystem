d4f602047b69fc5dd00cb8080a20ab73702d658c
razibdash <avrorazib@gmail.com>
2025-07-06 18:33:16 +0600

    Add test.py to invoke RemoteRunnable with sample input

---
+++ test.py
@@ -0,0 +1,5 @@
+from langserve import RemoteRunnable
+
+chain = RemoteRunnable("http://localhost:8000/chain/c/N4XyA")
+chain.invoke({"language": "French", "text": "Hello, how are you?"})
+print(chain.invoke({"language": "French", "text": "Hello, how are you?"}))
