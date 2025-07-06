a5f6f1dccc64022e667eba9ee1716dcd7dc64a16
razibdash <avrorazib@gmail.com>
2025-07-06 21:57:28 +0600

    Add Sumarization.py for text summarization API using FastAPI and LangChain

---
+++ Sumarization.py
@@ -0,0 +1,45 @@
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv, find_dotenv
+from fastapi import FastAPI
+from langserve import add_routes
+import uvicorn
+
+
+_ = load_dotenv(find_dotenv())
+GROQ_API_KEY = os.environ["GROQ_API_KEY"]
+
+
+model = ChatGroq(
+    api_key=GROQ_API_KEY,
+    model="llama3-70b-8192",
+)
+
+parser = StrOutputParser()
+
+system_template = "You are a professional text summarizer. Your job is to read long and complex texts and provide clear, concise, and accurate summaries. Your summaries must retain the key points, facts, and overall meaning of the original text, while removing redundancy and irrelevant details."
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+# Define the chain that combines the prompt, model, and parser
+chain = prompt_template | model | parser
+
+# Create a FastAPI app and add the routes for the chain
+app = FastAPI(
+  title="simpleTranslator",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+# Add the chain to the FastAPI app
+add_routes(
+    app,
+    chain,
+    path="/summarize",
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
