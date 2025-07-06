5f591bccaaeaa6d7ab4b7102ba4f96069cdca3cd
razibdash <avrorazib@gmail.com>
2025-07-06 18:34:57 +0600

    Update test.py to invoke RemoteRunnable with Bangla text

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from langserve import RemoteRunnable
 
 chain = RemoteRunnable("http://localhost:8000/chain/c/N4XyA")
 chain.invoke({"language": "French", "text": "Hello, how are you?"})
-print(chain.invoke({"language": "French", "text": "Hello, how are you?"}))
+print(chain.invoke({"language": "Bangla", "text": "Hello, how are you?"}))
